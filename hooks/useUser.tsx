f81064ffd13acd27ffb256f35b645b6f08f0259e
unknown <antonyponson@gmail.com>
2024-04-23 20:18:25 +0530

    user hook addedd

--- hooks/useUser.tsx
+++ hooks/useUser.tsx
@@ -2,19 +2,29 @@
 
 import { Subscription, UserDetails } from "@/types/types"
 import { User } from "@supabase/auth-helpers-nextjs"
+import { useSessionContext, useUser as useSupaUser } from "@supabase/auth-helpers-react"
 import { createContext } from "react"
 
 type UserContextType = {
   accessToken: string | null
   user: User | null
-  userDetails: UserDetails | null 
-  isLoading: boolean
+  userDetails: UserDetails | null  
+  isLoading: boolean  
   subscription: Subscription | null
 }
 
 export const userContext = createContext<UserContextType | undefined>(
   undefined
 )
+
+export interface  Props {
+  [propName: string] : any
+}
+
+export const MyUserContextProvider = (props: Props) =>{
+  const {session,isLoading:isLoadinUser,supabaseClient: supabase} =useSessionContext()
+  const user = 
+}
 
 
 
