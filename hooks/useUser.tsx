f599ad44a0d79d475012bd154bf1edec0bf16614
unknown <antonyponson@gmail.com>
2024-04-26 21:20:25 +0530

    user hook addedd

--- hooks/useUser.tsx
+++ hooks/useUser.tsx
@@ -2,30 +2,34 @@
 
 import { Subscription, UserDetails } from "@/types/types"
 import { User } from "@supabase/auth-helpers-nextjs"
-import { useSessionContext, useUser as useSupaUser } from "@supabase/auth-helpers-react"
-import { createContext } from "react"
+import {
+  useSessionContext,
+  useUser as useSupaUser,
+} from "@supabase/auth-helpers-react"
+import { createContext, useState } from "react"
 
 type UserContextType = {
   accessToken: string | null
   user: User | null
-  userDetails: UserDetails | null  
-  isLoading: boolean  
+  userDetails: UserDetails | null
+  isLoading: boolean
   subscription: Subscription | null
 }
 
-export const userContext = createContext<UserContextType | undefined>(
-  undefined
-)
+export const userContext = createContext<UserContextType | undefined>(undefined)
 
-export interface  Props {
-  [propName: string] : any
+export interface Props {
+  [propName: string]: any
 }
 
-export const MyUserContextProvider = (props: Props) =>{
-  const {session,isLoading:isLoadinUser,supabaseClient: supabase} =useSessionContext()
-  const user = 
+export const MyUserContextProvider = (Props: Props) => {
+  const {
+    session,
+    isLoading: isLoadinUser,
+    supabaseClient: supabase,
+  } = useSessionContext()
+
+  const user = useSupaUser
+  const accessToken = session?.access_token ?? null
+  const [isLoadingData, setIsLoadingData] = useState(false)
 }
-
-
-
-
