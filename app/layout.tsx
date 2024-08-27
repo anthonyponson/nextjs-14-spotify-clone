c37e5d59e0596143d7cd498809f8291bcf7784dc
unknown <antonyponson@gmail.com>
2024-08-27 19:56:40 +0530

    user hooks added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,15 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import './globals.css'
-import Sidebar from '@/components/Sidebar'
-import SupabaseProvider from '@/providers/SupabaseProviders'
+import type { Metadata } from "next"
+import { Inter } from "next/font/google"
+import "./globals.css"
+import Sidebar from "@/components/Sidebar"
+import SupabaseProvider from "@/providers/SupabaseProviders"
+import UserProvider from "@/providers/userProvider"
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
-  title: 'spotify',
-  description: 'listen to music on online for free',
+  title: "spotify",
+  description: "listen to music on online for free",
 }
 
 export default function RootLayout({
@@ -17,10 +18,12 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang='en'>
+    <html lang="en">
       <body className={inter.className}>
         <SupabaseProvider>
-          <Sidebar>{children}</Sidebar>
+          <UserProvider>
+            <Sidebar>{children}</Sidebar>
+          </UserProvider>
         </SupabaseProvider>
       </body>
     </html>
--- hooks/useUser.tsx
+++ hooks/useUser.tsx
@@ -6,7 +6,7 @@ import {
   useSessionContext,
   useUser as useSupaUser,
 } from "@supabase/auth-helpers-react"
-import { createContext, useEffect, useState } from "react"
+import { createContext, useContext, useEffect, useState } from "react"
 
 type UserContextType = {
   accessToken: string | null
@@ -78,4 +78,13 @@ export const MyUserContextProvider = (Props: Props) => {
   }
 
   return <UserContext.Provider value={value} {...Props} />
+}
+
+export const useUser = () => {
+  const context = useContext(UserContext)
+  if (context === undefined){
+    throw new Error('useUser must be used within a UserProvider')
+  }
+
+  return context
 }
