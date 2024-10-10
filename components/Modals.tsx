98a05485883d0015f593f1d6a0353c52859dca1a
unknown <antonyponson@gmail.com>
2024-10-10 20:58:30 +0530

    user hooks added

--- components/Modals.tsx
+++ components/Modals.tsx
@@ -1,7 +1,22 @@
-import React from 'react'
+import React from "react"
+import * as Dialog from "@radix-ui/react-dialog"
+import { IoMdClose } from "react-icons/io"
 
-export default function Modals() {
-  return (
-    <div>Modals</div>
-  )
+interface ModalPprops {
+  isOpen: boolean
+  onChange: (open: boolean) => void
+  title: string
+  description: string
+  children: React.ReactNode
 }
+
+const Modals: React.FC<ModalPprops> = ({
+  isOpen,
+  onChange,
+  title,
+  description,
+  children,
+}) => {
+  return <div>Modals</div>
+}
+export default Modals
