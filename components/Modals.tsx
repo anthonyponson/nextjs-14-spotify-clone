1ff65449df02afccf50fe939cb73a35ad5b12e10
unknown <antonyponson@gmail.com>
2024-10-22 20:36:52 +0530

    user hooks added

--- components/Modals.tsx
+++ components/Modals.tsx
@@ -1,13 +1,13 @@
-import React from "react"
-import * as Dialog from "@radix-ui/react-dialog"
-import { IoMdClose } from "react-icons/io"
+import React from "react";
+import * as Dialog from "@radix-ui/react-dialog";
+import { IoMdClose } from "react-icons/io";
 
 interface ModalPprops {
-  isOpen: boolean
-  onChange: (open: boolean) => void
-  title: string
-  description: string
-  children: React.ReactNode
+  isOpen: boolean;
+  onChange: (open: boolean) => void;
+  title: string;
+  description: string;
+  children: React.ReactNode;
 }
 
 const Modals: React.FC<ModalPprops> = ({
@@ -17,14 +17,16 @@ const Modals: React.FC<ModalPprops> = ({
   description,
   children,
 }) => {
-  return <Dialog.Root
-  open={isOpen}
-  defaultOpen={isOpen}
-  onOpenChange={onChange}
-  >
-    <Dialog.DialogPortal>
-      
-    </Dialog.DialogPortal>
-  </Dialog.Root>
-}
-export default Modals
+  return (
+    <Dialog.Root open={isOpen} defaultOpen={isOpen} onOpenChange={onChange}>
+      <Dialog.DialogPortal>
+        <Dialog.Overlay className="bg-neutral-900/90 back backdrop-blur-sm fixed inset-0 ">
+          <Dialog.Content className=" fixed drop-shadow-sm border border-neutral-700 top-[50%] left-[50%] max-h-full h-full md:h-auto md:max-h-[85vh] w-full md:w-[90vw] md:max-w-[450px]">
+            <p>hdilvhvi</p>
+          </Dialog.Content>
+        </Dialog.Overlay>
+      </Dialog.DialogPortal>
+    </Dialog.Root>
+  );
+};
+export default Modals;
