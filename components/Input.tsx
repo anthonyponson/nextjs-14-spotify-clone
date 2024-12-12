7f3501ecbab305ce6a10c19abb87730ffe8a34e3
unknown <antonyponson@gmail.com>
2024-12-12 21:46:38 +0530

    song upload modal added

--- components/Input.tsx
+++ components/Input.tsx
@@ -1,10 +1,42 @@
+import { forwardRef, HtmlHTMLAttributes } from "react";
 
-const Input = () => {
+
+interface InputProps 
+extends React.InputHTMLAttributes<HtmlHTMLAttributes>{}
+const Input = forwardRef<HTMLInputElement,InputProps>(({
+  className,
+  type,
+  disabled,
+  ...props
+},ref) => {
   return (
-    <>
-      <div></div>
-    </>
-  );
-}
+    <input
+    type={type}
+    className={`
+    flex
+    w-full
+    rounded-full
+    bg-neutral-700
+    border-2
+    border-transparent
+    px-3
+    py-2
+    text-sm
+    file:border-0
+    file:bg-transparent
+    file:text-sm
+    file:font-medium
+    placeholder:text-neutral-400
+    disabled:cursor-not-allowed
+    disabled:opacity-50
+    focus:outline-none
+    ${className}
+    `}
+    disabled={disabled}
+    ref={ref}
+    {...props}
+    />
+  )
+})
 
 export default Input;
