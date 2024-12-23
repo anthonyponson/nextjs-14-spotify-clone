d67ec49ea40cc3d3431eccd4d8fa3b34df469e2c
unknown <antonyponson@gmail.com>
2024-12-23 20:57:09 +0530

    search box added

---
+++ app/search/page.tsx
@@ -0,0 +1,30 @@
+import getSongsByTitle from "@/actions/getSongsByTitle"
+import ListItem from "@/components/ListItem"
+import PageContent from "../(site)/components/PageContent"
+import Header from "@/components/Header"
+import SearchInput from "@/components/SearchInput"
+
+interface SearchProps {
+  searchParams: {
+    title: string
+  }
+}
+
+const Search = async ({ searchParams }: SearchProps) => {
+  const songs = await getSongsByTitle(searchParams.title)
+
+  return (
+    <div className="bg-neutral-900 rounded-lg h-full w-full overflow-hidden overflow-y-auto">
+      <Header>
+        <div className="mt-2">
+          <h2 className="text-2xl font-semibold">Search</h2>
+          <SearchInput />
+          
+        </div>
+      </Header>
+
+    </div>
+  )
+}
+
+export default Search
---
+++ components/SearchInput.tsx
@@ -0,0 +1,10 @@
+
+const SearchInput = () => {
+  return (
+    <>
+      <div>search box</div>
+    </>
+  );
+}
+
+export default SearchInput;
