c309496ae37faa23953101a735f77b13cc390235
unknown <antonyponson@gmail.com>
2025-01-04 23:55:13 +0530

    liked button

--- components/LikeButton.tsx
+++ components/LikeButton.tsx
@@ -1,27 +1,46 @@
-'use client'
+"use client"
 
-import { useUser } from "@/hooks/useUser";
-import { useSession, useSessionContext } from "@supabase/auth-helpers-react";
-import { useRouter } from "next/navigation";
-import {useState, useEffect} from 'react'
+import { useUser } from "@/hooks/useUser"
+import { useSession, useSessionContext } from "@supabase/auth-helpers-react"
+import { useRouter } from "next/navigation"
+import { useState, useEffect } from "react"
 
-interface LikeButtonProps{
-    songId: string
+interface LikeButtonProps {
+  songId: string
 }
 
-const LikeButton:React.FC<LikeButtonProps> = ({songId}) => {
+const LikeButton: React.FC<LikeButtonProps> = ({ songId }) => {
+  const router = useRouter()
 
-    const router = useRouter()
+  const { supabaseClient } = useSessionContext()
 
-    const {supabaseClient} = useSessionContext()
+  const { user } = useUser()
 
-    const {user} = useUser()
+  const [liked, setLiked] = useState(false)
 
-  return (
-    <>
-      
-    </>
-  );
+  useEffect(() => {
+
+    if (!user?.id) {
+      return 
+    }
+      const fetchLiked = async () => {
+
+        const { data, error } = await supabaseClient
+          .from("liked_songs")
+          .select("*")
+          .eq("user_id", user.id)
+          .eq("song_id", songId)
+          .single()
+        if (!error &&data) {
+          setLiked(true)
+        }
+      }
+
+      fetchLiked()
+
+  }, [songId,supabaseClient, user?.id, ])
+
+  return <></>
 }
 
-export default LikeButton;
+export default LikeButton
