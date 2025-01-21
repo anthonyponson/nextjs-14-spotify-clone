66ad33330b1e4c8720329fe399136df803652c50
anthonyponson <antonyponson@gmail.com>
2025-01-21 19:26:39 +0530

    play player addded

--- hooks/userPlayer.ts
+++ hooks/userPlayer.ts
@@ -1,7 +1,20 @@
+import { create } from "zustand"
+
 interface PlayerStore {
-    ids: string[];
-    activId: string;
-    setId: (id: string) => void;
-    setIds: (ids: string[]) => void;
-    reset: () => void;
+  ids: string[]
+  activId: string
+  setId: (id: string) => void
+  setIds: (ids: string[]) => void
+  reset: () => void
 }
+
+const useplayer = create<PlayerStore>((set) => ({
+  ids: [],
+  activId: "",
+  setId: (id: string) => set({ activId: id }),
+  setIds: (ids: string[]) => set({ ids }),
+  reset: () => set({ ids: [], activId: undefined }),
+}))
+
+
+export default useplayer 
