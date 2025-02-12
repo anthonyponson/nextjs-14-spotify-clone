cbb16bf3052ff9002e5415520127bc5e6f6dda5a
anthonyponson <antonyponson@gmail.com>
2025-02-12 21:22:36 +0530

    player

--- components/PlayerContent.tsx
+++ components/PlayerContent.tsx
@@ -3,6 +3,14 @@
 import { Song } from "@/types/types"
 import MediaItem from "./MediaItem"
 import LikeButton from "./LikeButton"
+import {
+  BsPauseFill,
+  BsPlayFill,
+  BsVolumeMuteFill,
+  BsVolumeUpFill,
+} from "react-icons/bs"
+import { AiFillStepBackward } from "react-icons/ai"
+import Slider from "./Slider"
 
 interface PlayerContentProps {
   song: Song
@@ -10,12 +18,47 @@ interface PlayerContentProps {
 }
 
 const PlayerContent: React.FC<PlayerContentProps> = ({ song, songUrl }) => {
+  const Icon = true ? BsPauseFill : BsPlayFill
+  const VolumeIcon = true ? BsVolumeUpFill : BsVolumeMuteFill
   return (
-    <div className="flex items-center justify-between">
-      <div className="flex width-full justify-start">
+    <div className="grid grid-cols-3 md:grid-col-3 h-full">
+      <div className="flex w-full justify-start">
         <div className="flex items-center gap-x-4">
           <MediaItem data={song} />
           <LikeButton songId={song.id} />
+        </div>
+      </div>
+
+      <div className="flex md:hidden col-auto w-full justify-end items-center">
+        <div
+          onClick={() => {}}
+          className="h-10 w-10 items-center justify-center rounded-full bg-white p-1 cursor-pointer"
+        >
+          <Icon size={30} color="black" />
+        </div>
+      </div>
+
+      <div className="hidden md:flex h-full w-full justify-center items-center max-w-[722px] gap-x-6">
+        <AiFillStepBackward
+          size={30}
+          className="text-neutral-400 hover:text-white transition"
+          onClick={() => {}}
+        />
+        <div className="flex items-center justify-center h-10 w-10 rounded-full bg-white p-1 cursor-pointer">
+          <Icon size={30} color="black" />
+        </div>
+
+        <AiFillStepBackward
+          size={30}
+          className="text-neutral-400 hover:text-white transition"
+          onClick={() => {}}
+        />
+      </div>
+
+      <div className="hidden md:flex w-full justify-end pr-2">
+        <div className="flex items-center gap-x-2 w-[120px]">
+          <VolumeIcon onClick={() => {}} size={30} color="white" />
+            <Slider />
         </div>
       </div>
     </div>
---
+++ components/Slider.tsx
@@ -0,0 +1,30 @@
+import * as RadixSlider from '@radix-ui/react-slider';
+
+interface SliderProps {
+    value? : number;
+    onChange?: (value: number) => void;
+
+}
+const Slider:React.FC<SliderProps> = ({value = 1, onChange}) => {
+    const handleChange = (newValue: number[]) => {
+        onChange?.(newValue[0]);
+    }
+  return (
+   <div>
+    <RadixSlider.Root
+    className='relative flex items-center select-none touch-none w-full h-10'
+      defaultValue={[1]}
+      max={1}
+      step={0.1}
+      value={[value]}
+      onValueChange={handleChange}
+    >
+      <RadixSlider.Track className="relative flex-grow rounded-full h-2 bg-neutral-600">
+        <RadixSlider.Range className="absolute rounded-full h-full bg-white" />
+      </RadixSlider.Track>
+    </RadixSlider.Root>
+   </div>
+  )
+}
+
+export default Slider;
